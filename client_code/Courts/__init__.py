d005be2a204bf6db0624395bf7dd7e300eecb112
goodursus@gmail.com <goodursus@gmail.com>
2025-04-21 10:26:06 +0000

    Created Courts; Edited MainTemplate, settings and RotationPlayers

--- client_code/Courts/__init__.py
+++ client_code/Courts/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import MainTemplateTemplate
+from ._anvil_designer import CourtsTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,17 +8,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class MainTemplate(MainTemplateTemplate):
+class Courts(CourtsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def link_players_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('ListPlayers')
-
-  def link_courts_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('CopurtView')
