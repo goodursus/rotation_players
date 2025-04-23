8dac78b79164af942f680ffc53f7cdb8aeffdda9
goodursus@gmail.com <goodursus@gmail.com>
2025-04-23 12:37:20 +0000

    Edited MainTemplate

--- client_code/MainTemplate/__init__.py
+++ client_code/MainTemplate/__init__.py
@@ -1,4 +1,5 @@
-from ._anvil_designer import MainTemplateTemplate
+#from ._anvil_designer import MainTemplateTemplate
+#from ._anvil_designer import TokenBoxTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
