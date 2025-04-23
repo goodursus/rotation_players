93bc17a1647c3412b08bb3d3da5d635200b0bc1f
goodursus@gmail.com <goodursus@gmail.com>
2025-04-23 12:48:15 +0000

    Edited Courts

--- client_code/Courts/__init__.py
+++ client_code/Courts/__init__.py
@@ -2,9 +2,10 @@
 from ._anvil_designer import CourtsTemplate
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
+#import anvil.google.auth, anvil.google.drive
+#from anvil.google.drive import app_files
 import anvil.tables as tables
+from anvil.google.drive import app_files
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
