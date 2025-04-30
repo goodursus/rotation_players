a23eca119dcc998306b05e39a852c365acaac4d1
goodursus@gmail.com <goodursus@gmail.com>
2025-04-30 09:47:35 +0000

    Edited Courts

--- client_code/Courts/__init__.py
+++ client_code/Courts/__init__.py
@@ -16,3 +16,4 @@ class Courts(CourtsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.repeating_panel_player.items = app_tables.players.search()
